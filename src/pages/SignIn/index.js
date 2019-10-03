2c8155859805ffa134981da9d7250ac6a713f7a0
Vinicius <viniciusvicentini@live.com>
2019-10-02 21:23:01 -0300

    layout pages: signin signup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,54 +1,37 @@
-import React from 'react'
-
-import { Link, withRouter } from 'react-router-dom'
-
-import api from '../../services/api'
-import { login } from '../../services/auth'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import { Form, Container } from './styles'
 
-// import { Container } from './styles';
+export default function SignIn() {
+  const [email, setEmail] = useState([])
+  const [password, setPassword] = useState([])
 
-function SignIn() {
-  const handleSignIn = async e => {
+  const handleSignUp = e => {
     e.preventDefault()
-    const { email, password } = this.state
-    if (!email || !password) {
-      this.setState({ error: 'Preencha e-mail e senha para continuar!' })
-    } else {
-      try {
-        const response = await api.post('/sessions', { email, password })
-        login(response.data.token)
-        this.props.history.push('/app')
-      } catch (err) {
-        this.setState({
-          error:
-            'Houve um problema com o login, verifique suas credenciais. T.T',
-        })
-      }
-    }
   }
 
   return (
     <Container>
-      <Form onSubmit={handleSignIn}>
-        {this.state.error && <p>{this.state.error}</p>}
+      <Form onSubmit={handleSignUp}>
         <input
           type="email"
           placeholder="Endereço de e-mail"
-          onChange={e => this.setState({ email: e.target.value })}
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="Senha"
-          onChange={e => this.setState({ password: e.target.value })}
+          value={password}
+          onChange={e => setPassword(e.target.value)}
         />
-        <button type="submit">Entrar</button>
+
+        <button type="submit">Acessar conta</button>
+
         <hr />
-        <Link to="/signup">Criar conta grátis</Link>
+        <Link to="/signup">Criar conta</Link>
       </Form>
     </Container>
   )
 }
-
-export default withRouter(SignIn)
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,36 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { Form, Container } from './styles'
 
 export default function SignIn() {
+  const [username, setUsername] = useState([])
+  const [email, setEmail] = useState([])
+  const [password, setPassword] = useState([])
+
   const handleSignUp = e => {
     e.preventDefault()
-    alert('Eu vou te registrar')
   }
 
   return (
     <Container>
-      <Form onSubmit={() => handleSignUp}>
-        {this.state.error && <p>{this.state.error}</p>}
+      <Form onSubmit={handleSignUp}>
         <input
           type="text"
           placeholder="Nome de usuário"
-          onChange={e => this.setState({ username: e.target.value })}
+          value={username}
+          onChange={e => setUsername(e.target.value)}
         />
         <input
           type="email"
           placeholder="Endereço de e-mail"
-          onChange={e => this.setState({ email: e.target.value })}
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="Senha"
-          onChange={e => this.setState({ password: e.target.value })}
+          value={password}
+          onChange={e => setPassword(e.target.value)}
         />
-        <button type="submit">Cadastrar grátis</button>
+
+        <button type="submit">Criar conta</button>
+
         <hr />
-        <Link to="/">Fazer login</Link>
+        <Link to="/signin">Fzer login</Link>
       </Form>
     </Container>
   )
