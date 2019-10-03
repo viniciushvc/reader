2394cd6107f8c159b809572267c736e9e86034e2
Vinicius <viniciusvicentini@live.com>
2019-10-03 10:12:58 -0300

    hashing TOKEN_KEY

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,4 @@
-export const TOKEN_KEY = '@reader-Token'
+export const TOKEN_KEY = '1de9b0a30075ae8c303eb420c103c320'
 
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null
 
