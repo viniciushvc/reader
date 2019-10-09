4e874944b5bb56ab80c86da3391817e894c9e769
Vinicius <viniciusvicentini@live.com>
2019-10-09 18:34:38 -0300

    add img width 100% reader page

--- src/pages/Reader/styles.js
+++ src/pages/Reader/styles.js
@@ -3,6 +3,11 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   justify-content: center;
+  padding: 0 15px;
+
+  img {
+    width: 100%;
+  }
 
   h1 {
     margin-top: 2rem;
