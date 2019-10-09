a1cef1d59307552e6ad865b05d46bab7275794b5
Vinicius <viniciusvicentini@live.com>
2019-10-09 00:30:33 -0300

    fixing isAuthenticated method

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,13 +1,14 @@
 export const TOKEN_KEY = '1de9b0a30075ae8c303eb420c103c320'
 
-export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null
+export const isAuthenticated = () => {
+  const { user } = JSON.parse(localStorage.getItem('state'))
+
+  if (user) return user.isAuth
+  return false
+}
 
 export const getToken = () => localStorage.getItem(TOKEN_KEY)
 
-export const login = token => {
-  localStorage.setItem(TOKEN_KEY, token)
-}
+export const login = token => localStorage.setItem(TOKEN_KEY, token)
 
-export const logout = () => {
-  localStorage.removeItem(TOKEN_KEY)
-}
+export const logout = () => localStorage.removeItem(TOKEN_KEY)
